08393483c1d355b23e277ac29650196930ec74c4
Johan Kämpe <kampe.johan@gmail.com>
2018-07-01 09:24:28 +0200

    add mock-heroes.ts: containing an array of heroes of the 'Hero'-class

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero'; // import hero class
+
+// names from http://www.fantasynamegenerators.com/hero_names.php
+export const HEROES: Hero[] = [
+    { id : 11, name : "The Fancy Knuckles" },
+    { id : 12, name : "The Hot Soldier" },
+    { id : 13, name : "The Light Swallow" },
+    { id : 14, name : "The Fabulous Ant" },
+    { id : 15, name : "Doctor Storm Catman" },
+    { id : 16, name : "Blue Gloom" },
+    { id : 17, name : "Professor Fiery Raven" },
+    { id : 18, name : "Doctor Water Cheetah" },
+    { id : 19, name : "Starlight" },
+    { id : 20, name : "Misty Manta" }
+]
